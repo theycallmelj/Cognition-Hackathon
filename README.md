a2c30b26dc3da6287030611459a94312b01ed7eb
theycallmelj <43352450+theycallmelj@users.noreply.github.com>
2024-05-25 13:59:04 -0400

    added some soul

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Cognition-Hackathon
-
+The opensoulcode directory is for sandbox code and setting up the soul.
+The call-llm was taken from the eleven public repo and then modified to use the new soul code.
 
 The goal is to link up witn this (repo)[https://github.com/elevenlabs/elevenlabs-examples/blob/main/examples/twilio/call-llm/src/llm.ts]
 
--- call-llm/src/llm.ts
+++ call-llm/src/llm.ts
@@ -1,27 +1,23 @@
 import EventEmitter from 'events';
 import OpenAI from 'openai';
+import { Soul, said } from "@opensouls/soul";
 
 export class Llm extends EventEmitter {
-  private openai: OpenAI;
+  private soul: Soul;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private userContext: any[];
-  private partialResponseIndex: number;
 
   constructor() {
     super();
-    this.openai = new OpenAI();
-    this.userContext = [
-      {
-        role: 'system',
-        content:
-          'You are a helpful assistant called Eleven. You help with anything related to ElevenLabs. ElevenLabs is an AI research company that develops audio AI models. Keep your responses short and to the point. This is a conversation you are having with a person, so keep it natural and casual.',
-      },
-      {
-        role: 'assistant',
-        content: 'Hi, my name is Eleven. How can I help you?',
-      },
-    ];
-    this.partialResponseIndex = 0;
+    this.soul = new Soul({
+      organization: "capsaicinkidliamjohnston",
+      blueprint: "customer-service-broker",
+    });
+    this.userContext = [];
+    //callback for completion
+    this.soul.on("says", async ({ content }) => {
+      this.emit('llmreply', await content());
+    });
   }
 
   // Add the callSid to the chat context in case
@@ -40,40 +36,8 @@ export class Llm extends EventEmitter {
 
   async completion(text: string, role = 'user', name = 'user') {
     this.updateUserContext(name, role, text);
-
-    // Step 1: Send user transcription to LLM
-    const stream = await this.openai.chat.completions.create({
-      model: 'gpt-3.5-turbo-0125',
-      messages: this.userContext,
-      stream: true,
-    });
-
-    let completeResponse = '';
-    let partialResponse = '';
-    let finishReason = '';
-
-    for await (const chunk of stream) {
-      const content = chunk.choices[0]?.delta?.content || '';
-
-      finishReason = chunk.choices[0]?.finish_reason ?? '';
-
-      // We use completeResponse for userContext
-      completeResponse += content;
-      // We use partialResponse to provide a chunk for TTS
-      partialResponse += content;
-      // Emit last partial response and add complete response to userContext
-      if (content.trim().slice(-1) === '•' || finishReason === 'stop') {
-        const llmReply = {
-          partialResponseIndex: this.partialResponseIndex,
-          partialResponse,
-        };
-
-        this.emit('llmreply', llmReply);
-        this.partialResponseIndex++;
-        partialResponse = '';
-      }
-    }
-    this.userContext.push({ role: 'assistant', content: completeResponse });
+    await this.soul.dispatch(said("User", text)); 
+    this.userContext.push({ role: 'assistant', content: this.userContext + text});
     console.log(`LLM -> user context length: ${this.userContext.length}`.green);
   }
 }
